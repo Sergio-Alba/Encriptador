e65ddd9e4f85132d3102d072175e03a0f480190f
Sergio Alba <sergioalba.1099@gmail.com>
2024-03-20 12:24:41 -0400

    añadiendo los Estilos para tabled y pc

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const $insertText = document.getElementById('insert-text').value
+const $btnEncriptar = document.getElementById('btn-encriptar')
+const $btnDesencriptar =document.getElementById('btn-desencriptar')
+const $btnCopy =document.getElementById('btn-copy')
+
+
+// función para encriptar el texto ingresado
+
